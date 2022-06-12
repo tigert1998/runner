33aaa1120cb1e6682f6a849e07e7eba893595895
tigertang <tigertang.zju@qq.com>
2022-06-13 03:14:39 +0800

    Add save_best option in CheckpointHook

--- runner/hooks.py
+++ runner/hooks.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+from glob import glob
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -109,18 +110,94 @@ class TensorboardLoggerHook(LoggerHook):
 
 
 class CheckpointHook(Hook):
+    def __init__(self, save_best=None, compare_op="greater"):
+        super().__init__()
+        self.save_best = save_best
+        self.compare_op = compare_op
+        self.best_score = {}
+        self.best_ckpt_path = {}
+
+    def compare(self, x, y):
+        if self.compare_op == "greater":
+            return x > y
+        elif self.compare_op == "less":
+            return x < y
+
+    @staticmethod
+    def _save_checkpoint(work_dir, state_dict, optimizer, lr_scheduler, epoch, meta, template="epoch_{}.pth"):
+        path = osp.join(work_dir, template.format(epoch))
+        torch.save({
+            "meta": {"epoch": epoch, **meta},
+            "state_dict": state_dict,
+            "optimizer": optimizer.state_dict(),
+            "lr_scheduler": lr_scheduler.state_dict(),
+        }, path)
+
     @master_only
     def after_train_epoch(self, runner):
-        path = osp.join(runner.work_dir, f"epoch_{runner.epoch + 1}.pth")
         model = runner.model.module if is_distributed() else runner.model
-        torch.save(
-            {
-                "optimizer": runner.optimizer.state_dict(),
-                "lr_scheduler": runner.lr_scheduler.state_dict(),
-                "state_dict": model.state_dict(),
-            },
-            path
+        self._save_checkpoint(
+            runner.work_dir,
+            model.state_dict(),
+            runner.optimizer,
+            runner.lr_scheduler,
+            runner.epoch + 1, {}
         )
         if runner.epoch - 4 >= 1:
             os.remove(osp.join(runner.work_dir,
                       f"epoch_{runner.epoch - 4}.pth"))
+
+    @master_only
+    def after_val_epoch(self, runner):
+        if self.save_best is None:
+            return
+
+        flow = runner.flow.split('/')[-1]
+        model = runner.model.module if is_distributed() else runner.model
+
+        if self.best_score.get(flow, None) is None:
+            # try to find the checkpoint with the best score
+            paths = glob(
+                f"{runner.work_dir}/{flow}_best_{self.save_best}_epoch_*.pth"
+            )
+            for path in paths:
+                try:
+                    ckpt = torch.load(path, map_location="cpu")
+                except:
+                    continue
+                if ckpt.get("meta", None) is not None and \
+                        ckpt["meta"].get(flow, None) is not None and \
+                        ckpt["meta"][flow].get(self.save_best, None) is not None and \
+                        (self.best_score.get(flow, None) is None or
+                            self.compare(ckpt["meta"][flow][self.save_best], self.best_score[flow])):
+                    self.best_score[flow] = ckpt["meta"][flow][self.save_best]
+                    self.best_ckpt_path[flow] = path
+
+            if self.best_score.get(flow, None) is not None:
+                runner.logger.info(
+                    f"Found the best ckpt at \"{self.best_ckpt_path[flow]}\".")
+
+            for path in paths:
+                if path != self.best_ckpt_path.get(flow, None):
+                    os.remove(path)
+
+        if self.best_score.get(flow, None) is None or \
+                self.compare(runner.log_buffer.output[self.save_best], self.best_score[flow]):
+            self.best_score[flow] = runner.log_buffer.output[self.save_best]
+            prev_best_ckpt_path = self.best_ckpt_path.get(flow, None)
+            self.best_ckpt_path[flow] = osp.join(
+                runner.work_dir,
+                f"{flow}_best_{self.save_best}_epoch_{runner.epoch}.pth"
+            )
+            self._save_checkpoint(
+                runner.work_dir,
+                model.state_dict(),
+                runner.optimizer,
+                runner.lr_scheduler,
+                runner.epoch,
+                {flow: runner.log_buffer.output},
+                f"{flow}_best_{self.save_best}"
+                "_epoch_{}.pth"
+            )
+            if prev_best_ckpt_path is not None:
+                os.remove(prev_best_ckpt_path)
